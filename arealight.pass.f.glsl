bde4523315b3ffad92dc91020c29cca6278cef07
WILLIAMNGUYEN18 <nguyew2@uw.edu>
2018-07-24 17:54:25 -0700

    Reviewed Code up to Intensity Calculations

--- arealight.pass.f.glsl
+++ arealight.pass.f.glsl
@@ -1,44 +1,36 @@
 precision highp float;
 precision highp int;
 
-//vertex for surface being shaded
 in vec3 vpos;
-
-//vertex normal
 in vec3 vnormal;
 
-//directional vector of light
-//is eone calculated from lpos - vert?;
-in vec3 eone;
+in vec3 vcolor;
+in vec3 wcoord;
+in vec3 wvnormal;
+in vec3 vdepth;
 
-
-//radius of area light
-in float rone;
-
-//point in cener of area light, intensity in pointlight[3]
+uniform vec3 eone;
+uniform float rone;
+uniform float exposure;
 uniform vec4 pointlight;
-
-
-
 
 out vec4 color;
 
 void main() {
     vec3 bright = vec3(0,0,0);
     float pi = 3.1415926535897932384626433832795;
-    float incdegree = (2 * pi) / 360;
+    float incdegree = pi / 12;
     vec3 z = vec3(0,0,1);
 
     //center point of area light based on pointlight vector
-    vec3 lpos = vec3(pointlight[0], pointlight[1], pointlight[2]);
+    vec3 l = vec3(pointlight[0] - vpos[0], pointlight[1] - vpos[1], pointlight[2] - vpos[2]);
 
 
 
     //vcolor copied from pass.f.glsl
     vec3 colorrgb = vec3(115.0/255.0, 161.0/255.0, 227.0/255.0);
-    colorrgb = clamp(colorrgb, 0.0, 1.0);
 
-    for (float r = 0; r < rone; r+= 0.01){
+    for (float r = 0; r < rone; r+= 0.1){
         for(float theta = 0; theta < 2 * pi; theta += incdegree){
 
             //pp
@@ -51,31 +43,32 @@ void main() {
             plane = normalize(plane);
 
             //rotation angle calculation
-            float rot = acos( dot(eone/length(eone),z / length(z)) / (
-                length(eone)*length(z)) );
+            float rot = acos( dot(eone/length(eone),z / length(z)) );
 
-            //rotation vector of pp + transform (lpos)
-            vec3 Wpp = vec3(
+            //rotation vector of pp + transform (l)
+            vec3 Wpp =
                 pp * cos(rot)  
-                + normalize(cross(plane/length(plane), pp/ length(pp)))
-                + plane * (dot(plane / length(plane),pp / length(pp))) * (1-cos(rot)) )
-            + lpos;
+                + normalize(cross(plane, 
+                pp))
+                + plane * (dot(plane,
+                pp)) * (1-cos(rot)) 
+                + l;
  
             //scale by area divided by number of samples
             //Light intensity calculation//pointlight[3] = inherent light intensity  
-            float intense = (pi * (pow(rone,2))) / ( (rone / 0.01)  *   ((2*pi) / incdegree) );
-            intense *= pointlight[3] * dot(-lpos / length(lpos)), eone / length(eone)); 
+            float intense = (pi * (pow(rone,2))) / ( (rone / 0.1)  *   ((2*pi) / incdegree) );
+            intense *= pointlight[3] * dot(-l / length(l)), eone / length(eone)); 
 
             //intensity is multiplied into equation
-            Wpp *= intense;
+            //Wpp *= intense;
             
             //equation is multiplied into rgb values
-            colorrgb *= Wpp;
-            colorrgb = clamp(colorrgb, 0.0, 1.0);
+            //colorrgb *= Wpp;
+            //colorrgb = clamp(colorrgb, 0.0, 1.0);
+            
 
             //diffuse highlight
             vec3 tmp = (normalize(vnormal));
-            vec3 l = vec3(pointlight[0] - vpos[0], pointlight[1] - vpos[1], pointlight[2] - vpos[2]);
             colorrgb *= dot(tmp , l / length(l));
             colorrgb /= length(l) * length(l);
             colorrgb = clamp(colorrgb, 0.0, 1.0);
