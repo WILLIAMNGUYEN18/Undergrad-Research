168143e8cc6f9b0bc29200310655cbed91e7069b
WILLIAMNGUYEN18 <nguyew2@uw.edu>
2018-07-26 19:45:18 -0700

    Changed code structure to match integrate.dblquad. Unsure of where alpha, beta, and w variables come from

--- sumlight.py
+++ sumlight.py
@@ -1,7 +1,9 @@
-msg = "test123"
-print(msg)
 from scipy import integrate
+from scipy.integrate import quad, dblquad
 import math
+#msg = "test123"
+#print(msg)
+
 
 # example double integral
 #f = lambda y, x: x*y**2
@@ -20,11 +22,31 @@ def Rect(n):
         return 0
 
 
-
-def func(alpha, beta):
+#might not do alpha beta, may have to use the 2 range variables used in double variable.
+def test1(alpha, beta):
+    print ("fuck")
     #return math.cos(theta) * Rect((alpha - theta)/w) * Rect((beta - theta)/w)
     #math.cos(theta)
     #need to pull theta somehow
     #print("turn this into necessary function")
+
+#range for theta
+a = 0
+b = math.pi
+
+#range for psi
+gfun = 0
+hfun = 2 * math.pi
+
+
+#scipy.integrate.dblquad(func, a, b, gfun, hfun
+#1st parameter func 
+def func (theta, psi):
+    return math.cos(theta) * Rect(alpha - theta / w) * Rect(beta - theta /w)
+
+integrate.dblquad(func, a, b,gfun, hfun)
+
+
+
 
 
