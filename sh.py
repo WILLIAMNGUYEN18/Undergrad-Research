77a6025268648a40b8df9c5871ca0f9e2a55a358
WILLIAMNGUYEN18 <nguyew2@uw.edu>
2018-09-18 13:56:48 -0700

    All calculated values now matching precomputed values for spherical harmonics.

--- sh.py
+++ sh.py
@@ -15,13 +15,13 @@ import matplotlib.mlab
 import pyshtools
 
 
-theta = np.linspace(0, np.pi / 2, 24)
-phi = np.linspace(0, 2 * np.pi, 24)
+theta = np.linspace(0, np.pi / 2, 12)
+phi = np.linspace(0, 2 * np.pi, 12)
 
 #theta1, phi1 = np.meshgrid(theta, phi)
 m = 0
 l = 0
-w = np.pi / 24.0
+w = np.pi / 22.0
 
 sampleset = []
 
@@ -68,6 +68,7 @@ while l < 3:
     overtheta = [0,0,0]
     angphi = 0
     angtheta = 0
+    sphcoscalc = []
     while angtheta <= math.pi / 2:
         print("l = : " + str(l))
         print("phi = : " + str(angphi))
@@ -78,22 +79,36 @@ while l < 3:
         print("Rect Legendre function = : " + str(store2.tolist()))
         over1 = overall(l,m,w,angtheta, angphi)
         print("Cosine and Rectangle Spherical Harmonic Result: " + str(over1))
-        sphcoscalc.append( normalization(l) * eulers(m, phi) * lpmv(m,l, math.cos(angtheta)) )
-        
+        sphcoscalc.append( normalization(l) * eulers(m, angphi) * lpmv(m,l, math.cos(angtheta)) )            
         #list index out of range
         overtheta[l] += over1
-
-
-
-        angtheta += w
         angphi += w
+        angtheta += w
+
     #comparing spherical harmonic values to my result
-    sphcosharm = sph_harm(m, l,  phi, theta)
+    sphcosharm = sph_harm(m, l,  phi, theta).real
     print("Our calculated spherical harmonic of cos(theta) is: " + str(sphcoscalc))
     print("The pre-computed spherical harmonic of cos(theta) is:" + str(sphcosharm))
-    if sphcoscalc == sphcosharm:
-        print("Our calculation matches the precomputed spherical harmonic")
 
+    #QUASHED
+    #Only getting 12 calculated values for the 24 precomputed. 
+    #iterating over angphi would result in n^2 rather than double
+    #cut iterations of linspace from 24 to 12.
+
+    #
+
+    print ("length of calculated: " + str(len(sphcoscalc)))
+    print ("length of precomputed: " + str(len(sphcosharm)))
+    for i in range(len(sphcoscalc)):
+        if sphcoscalc[i] == sphcosharm[i]:
+            print("MATCHING CALCULATED & PRECOMPUTED AT: " + str(i))
+        else:
+            print("NOT MATCHING CALCULATING & PRECOMPUTED: " + str(i))
+    for x in np.nditer(theta):
+        print(x)
+    
+    for y in np.nditer(phi):
+        print (y)
     l += 1
 
 
