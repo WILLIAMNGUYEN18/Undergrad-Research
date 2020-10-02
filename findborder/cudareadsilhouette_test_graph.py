e435d78f5333fed8c07fc36fcc655e6c81088628
William Nguyen <nguyew2@uw.edu>
2020-10-01 23:10:22 -0700

    Converted structure to a map. Slower setup, faster execution. Running into same issue. Need to try BFS and then a different DFS

--- findborder/cudareadsilhouette_test_graph.py
+++ findborder/cudareadsilhouette_test_graph.py
@@ -10,112 +10,71 @@ def cudaPlotSilhouette(png):
     #4 channel RGBA image represented as a numpy array
     #img[r,c] = [b, g, r, a], img[r,c,3] = alpha channel
     img = cv2.imread(png, -1)
-    #print(str(img))
     plypoints = []
-    #traverse array until you find a border pixel, i.e. pixel where any one of the 8 neighbors has an alpha value of 0
-    #repeat until you get back to where you started:
-        #find neighbor that is also a border pixel
-        #append neighbor as array [r,c] to array plypoints
-        #handle edge cases / dead ends, but there should be no holes and only one path around the subject
 
-    # Find first pixel with alpha value 1; our starting point
-    # Start from top left, going down every coloumn
-    first_border = None
-    for r in range(0, img.shape[0]):
-        for c in range(0, img.shape[1]):
-
-            if img[r,c,3] != 0:
-                first_border = (r,c)
-                break
-
-        # Exit after finding the first border pixel
-        if first_border is not None:
-            break
-    print("first border pix: " + str(first_border))
-    # Case where there is no pixel with alpha value 1
-    if first_border is None:
-        print("No pixel with alpha value 1!")
-        exit()
-  
     # Used to loop through neighbors, counterclockwise
     #row,column, (Y,X)
     #east, southeast, south, southwest, west, northwest, north, northeast
     bd = [[0,1], [-1,1], [-1,0], [-1,-1], [0, -1], [1,-1], [1,0], [1,1]]
-
-
     point_map = {}
+    first = True
+    
+    print("Generating Graph")
+    #iterate through the entire map to create a full graph. We can use find_border_children and add them to the map
+    #this will allow us to find a comprehensive graph/mapping that we can iterate through
+    #let's still store first point as a starting point for the loop
+    for r in range(0, img.shape[0]):
+        for c in range(0, img.shape[1]):
+            currP = (r,c)
+            #if alpha value 1 (border)
+            #find all borders around this one and map it
+            if img[r,c,3] != 0 and is_border_pixel(img,currP, bd):
+                
+                if(first):
+                    firstPoint = currP
+                    first = False
+                children = find_border_children(img, currP, bd)
+                point_map[currP] = children
+    if firstPoint is None:
+        print("No pixel values with value 1")
+        exit()
+
     seen_points = set()
-    seen_points.add(first_border)
+    seen_points.add(firstPoint)
+    currPoint = firstPoint
     point_list = []
-    point_list.append(first_border)
-    pop_list = []
-    seen_list = []
-    streak_pop = []
 
-    curr = first_border
 
-    nonStreakPop = True
-
-    #Loop until we find first point again.
+    print("Exploring graph")
+    #Calvin implementation with direction reversed
+    #his was supposed to be clockwise but it went reverse
     while True:
-        children = find_border_children(img, curr, bd)
-
-        point_map[curr] = children
-
         next = None
-        #print("looping through children:" + str(children))
-        #it loops through all children not in seen_points, and then iterates through it, so it might be going in reverse order
-        for i, child in enumerate(children):
+        currChildren = point_map[currPoint]
+        for i, child in enumerate(currChildren):
             if child not in seen_points:
-                #print(str(i) + ": " + str(child))
-                # Found next point
                 next = child
-        
-        
-        # Case where there are no children; we go backwards
-        if next is None:
-
-            # Check if we are in range of starting pixel. If we are, then we are done
-            if points_in_range(first_border, curr, bd):
-                # This line appends the start point again, which i assume should not happen
-                # point_list.append(first_border)
                 break
-
-            # Edge case where first_border is the only pixel with alpha = 1 in range
+        if next is None:
+            if points_in_range(firstPoint, currPoint, bd):
+                break
             if len(point_list) == 1:
                 print("first_border is the only pixel with alpha = 1 in range")
                 exit()
-
-            # go back and get rid of last element
-            curr = point_list.pop()
-            if(nonStreakPop):
-                streak_pop.append(curr)
-                nonStreakPop = False
-            pop_list.append(curr)
-
+            currPoint = point_list.pop()   
         else:
-            # Found next point
-            # Append and move on to next set of children
             point_list.append(next)
             seen_points.add(next)
-            seen_list.append(next)
-            curr = next
-            if(nonStreakPop == False):
-                nonStreakPop = True
+            currPoint = next
 
 
-    print( "border pixel found: " + str(curr))
-    #print(seen_list)
-    print(len(pop_list))
-    print(len(point_list))
-    print(len(streak_pop))
     print(point_map)
-    num = 1
-    for p in seen_list:
-        img = cv2.circle(img, (p[1],p[0]), 20, (0,255,255), 1)
-        num = num + 1
-    #cv2.imshow('image', img)
-    fileName = "C:\\Users\\Brilliance\\Desktop\\Projects\\Undergrad-Research\\findborder\\result_circles.png"
+    #print(len(point_map.keys()))
+    #print(len(point_map.values()))
+    print(point_list)
+    print("first pixel: " + str(firstPoint))
+    print("last pixel: " + str(currPoint))
+    fileName = "C:\\Users\\Brilliance\\Desktop\\Projects\\Undergrad-Research\\findborder\\result_graph.png"
     cv2.imwrite(fileName,img)
 
 # Find neighboring points that are also border pixels
