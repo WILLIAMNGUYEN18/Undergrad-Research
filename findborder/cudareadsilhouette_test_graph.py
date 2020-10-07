f962e2a3a64b04baaf10af542060c87f89be591f
WILLIAMNGUYEN18 <nguyew2@uw.edu>
2020-10-06 22:53:11 -0700

    Able to check for point list max length and save that to see where it fails. Also able to create RGB in new image of same size. It seems that it fails near the beginning because it doesn't end on a node that is 1 from the ebeginning. with cardinal it will handle this since we end when there's no next and we still check all 8 blocks for the original (which will be diagonal for the edge case that failed).

--- findborder/cudareadsilhouette_test_graph.py
+++ findborder/cudareadsilhouette_test_graph.py
@@ -43,6 +43,11 @@ def cudaPlotSilhouette(png):
     seen_points.add(firstPoint)
     currPoint = firstPoint
     point_list = []
+    maxpoint_list = []
+    maxlistlength = len(point_list)
+    maxpoint = firstPoint
+    pop_list = []
+    seen_list = []
 
 
     print("Exploring graph")
@@ -61,23 +66,61 @@ def cudaPlotSilhouette(png):
             if len(point_list) == 1:
                 print("first_border is the only pixel with alpha = 1 in range")
                 exit()
-            currPoint = point_list.pop()   
+            if(maxlistlength < len(point_list)):
+                maxlistlength = len(point_list)
+                maxpoint_list = point_list.copy()
+                maxpoint = currPoint    
+            currPoint = point_list.pop()
+            pop_list.append(currPoint)   
         else:
             point_list.append(next)
             seen_points.add(next)
             currPoint = next
+            seen_list.append(next)
 
 
-    print(point_map)
+    #print(point_map)
     #print(len(point_map.keys()))
     #print(len(point_map.values()))
-    print(point_list)
+    #print(point_list)
     print("point_map size: " + str(len(point_map)))
     print("point_list size: " + str(len(point_list)))
     print("first pixel: " + str(firstPoint))
     print("last pixel: " + str(currPoint))
+    print(maxpoint_list)
+    print("maxpoint: " + str(maxpoint))
+    print("maxlist length: " + str(maxlistlength))
     fileName = "C:\\Users\\willi\\Desktop\\Programming\\Undergrad-Research\\findborder\\result_graph.png"
     cv2.imwrite(fileName,img)
+    outlinefileName = "C:\\Users\\willi\\Desktop\\Programming\\Undergrad-Research\\findborder\\resultgraph_outline.png"
+    outlineImage = np.zeros(shape=[img.shape[0],img.shape[1], 3], dtype=np.uint8)
+
+    for p in seen_list:
+        outlineImage[p[0],p[1],0] = 0
+        outlineImage[p[0],p[1],1] = 250
+        outlineImage[p[0],p[1],2] = 0
+
+    print("seen_list length: " + str(len(seen_list)))
+    print("pop_list length: " + str(len(pop_list)))
+    cv2.imwrite(outlinefileName, outlineImage)
+    outline_pop_filename = "C:\\Users\\willi\\Desktop\\Programming\\Undergrad-Research\\findborder\\resultgraph_outlinepop.png"
+    for p in pop_list:
+        outlineImage[p[0],p[1],0] = 0
+        outlineImage[p[0],p[1],1] = 0
+        outlineImage[p[0],p[1],2] = 250
+
+    cv2.imwrite(outline_pop_filename, outlineImage)
+    outline_maxlist_filename = "C:\\Users\\willi\\Desktop\\Programming\\Undergrad-Research\\findborder\\resultgraph_outlinemaxlist.png"
+    
+    n = 0
+    for p in maxpoint_list:
+        n = n + 1
+        variance = (255 / len(maxpoint_list)) * n
+        outlineImage[p[0],p[1],0] = variance
+        outlineImage[p[0],p[1],1] = 255 - variance
+        outlineImage[p[0],p[1],2] = 250
+
+    cv2.imwrite(outline_maxlist_filename, outlineImage)
 
 # Find neighboring points that are also border pixels
 def find_border_children(img, point, bd):
@@ -127,4 +170,4 @@ def points_in_range(p1, p2, bd):
 #C:\\Users\\willi\\Desktop\\Programming\\Undergrad-Research\\findborder\\mask0002.png
 #C:\\Users\\Brilliance\\Desktop\\Projects\\Undergrad-Research\\findborder\\mask0002.png
 if __name__ == "__main__":
-    cudaPlotSilhouette("C:\\Users\\willi\\Desktop\\Programming\\Undergrad-Research\\findborder\\mask0001.png")
+    cudaPlotSilhouette("C:\\Users\\willi\\Desktop\\Programming\\Undergrad-Research\\findborder\\mask0002.png")
